a846b7deb1c12aaf333177fc2d7db008b40a216b
Aliaksei <avkomosko@gmail.com>
2021-08-02 22:13:37 +0300

    init: start human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+  let ones = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'sven',
+    8: 'eight',
+    9: 'nine'
+  };
 }
