8aab71899159846cc1dc1675e093a823157c31b2
Aliaksei <avkomosko@gmail.com>
2021-08-04 00:42:21 +0300

    feat: implement function to work with numbers below hundreed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 module.exports = function toReadable (number) {
-  let ones = {
-    0: 'zero',
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    4: 'four',
-    5: 'five',
-    6: 'six',
-    7: 'sven',
-    8: 'eight',
-    9: 'nine'
-  };
+  let result;
+  let ones = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'};
+  let teens = {10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'};
+  let tens = {20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'};
+  let str = number.toString();
+  
+  if (str.length === 1) {
+    result = ones[number];
+  } else if (str.length === 2) {
+     if (number >= 10 && number < 20) {
+      result = teens[number];
+    } else if (number >= 20 && number < 100 && str[1] === '0') {
+      result = tens[number];
+      } else if (number < 100) { 
+        result = `${tens[Math.floor(number/10)*10]} ${ones[(number % 10)]}`;
+        }  
+  } else if (str[1] === '0' && str[2]=== '0') {
+    result = `${ones[Math.floor(number/100)]} hundred`;
+  } else if (str[1] !== '0' && str[2]=== '0') {
+    result = `${ones[Math.floor(number/100)]} hundred ${tens[number]}`;
+  } else {
+    result = `${ones[Math.floor(number/100)]} hundred ${toReadable((number % 100))}`;
+  }
+  return result;
 }
+    
+
